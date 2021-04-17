d373fc37352a0bcdf0278be6a44f64cc060b27be
pominot <ppminet@protonmail.com>
2021-04-17 19:33:06 +0700

    first commit

---
+++ src/components/Illustration.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col justify-center w-28 h-32 px-2 bg-gray-200 rounded-md shadow-lg">
+    <div class="mb-2">
+      <div class="w-4 h-4 bg-green-700 rounded-full"></div>
+    </div>
+    <div>
+      <div class="bg-blue-600 mb-2 h-2 w-3/4 rounded"></div>
+      <div class="bg-blue-600 mb-2 h-2 w-2/4 rounded"></div>
+      <div class="bg-blue-600 mb-2 h-2 w-full rounded"></div>
+      <div class="bg-blue-600 mb-2 h-2 w-1/4 rounded"></div>
+      <div class="bg-blue-600 mb-2 h-2 w-3/4 rounded"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Illustration'
+});
+</script>
---
+++ src/routes/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="font-inter h-screen grid place-items-center">
+    <div class="flex flex-col items-center">
+      <illustration class="transform origin-center -rotate-12 scale-90 md:scale-100" />
+
+      <div class="mt-6 text-center">
+        <h1 class="font-bold text-blue-800 text-4xl">Answers!</h1>
+        <p class="px-10 text-gray-500">Create and share your multiple choice answers <span class="underline font-bold">free</span>, no login required.</p>
+      </div>
+
+      <div class="mt-6">
+        <button type="button" class="btn">Create new!</button>
+        <span class="mx-1"></span>
+        <button type="button" class="btn">How to use</button>
+      </div>
+
+      <div class="mt-6">
+        <a class="text-blue-600" href="https://bimsua.me/privacy">Terms of use</a>
+        <span class="mx-3">|</span>
+        <a class="text-blue-600" href="https://bimsua.me/privacy">Privacy Policy</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Illustration from '../components/Illustration.vue';
+
+export default defineComponent({
+  name: 'HomeScreen',
+  components: { Illustration }
+});
+</script>
