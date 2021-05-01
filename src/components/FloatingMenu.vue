ddda7b03ba8bba3949c27ebfdb27dcaa90136ee6
pominot <ppminet@protonmail.com>
2021-05-01 18:34:34 +0700

    placeholder actions

--- src/components/FloatingMenu.vue
+++ src/components/FloatingMenu.vue
@@ -15,13 +15,15 @@
       </button>
     </div>
     <div class="fixed px-2 top-0 left-0 z-50 w-full h-full grid place-items-center" v-else>
-      <div data-flip-key="dialog" class="p-5 bg-gray-600 bg-opacity-75 rounded-lg backdrop-filter backdrop-blur-lg">
-        <h1 class="text-xl font-bold text-white">Action Menu</h1>
-        <p class="text-gray-200 text-sm">Chooose an action to do with this answer sheet</p>
-        <div class="flex flex-col my-6 space-y-2">
-          <button type="button" class="text-white border border-gray-400 hover:bg-gray-100 focus:outline-none hover:bg-opacity-25 focus:bg-gray-100 focus:bg-opacity-25 py-1 px-2 rounded">Save and return</button>
-          <button type="button" class="text-white border border-gray-400 hover:bg-gray-100 focus:outline-none hover:bg-opacity-25 focus:bg-gray-100 focus:bg-opacity-25 py-1 px-2 rounded">Discard this sheet</button>
-          <button type="button" class="text-white border border-gray-400 hover:bg-gray-100 focus:outline-none hover:bg-opacity-25 focus:bg-gray-100 focus:bg-opacity-25 py-1 px-2 rounded" @click="expanded = false">Return</button>
+      <div data-flip-key="dialog" class="p-5 overflow-hidden bg-gray-600 bg-opacity-75 rounded-lg backdrop-filter backdrop-blur-lg">
+        <div data-inverse-flip>
+          <h1 class="text-xl font-bold text-white">Action Menu</h1>
+          <p class="text-gray-200 text-sm">Chooose an action to do to this answer sheet</p>
+          <div class="flex flex-col my-6 space-y-2">
+            <button type="button" class="text-white border border-gray-400 hover:bg-gray-100 focus:outline-none hover:bg-opacity-25 focus:bg-gray-100 focus:bg-opacity-25 py-1 px-2 rounded" @click="$router.push('/')">Save and return</button>
+            <button type="button" class="text-white border border-gray-400 hover:bg-gray-100 focus:outline-none hover:bg-opacity-25 focus:bg-gray-100 focus:bg-opacity-25 py-1 px-2 rounded" @click="$router.push('/')">Discard this sheet</button>
+            <button type="button" class="text-white border border-gray-400 hover:bg-gray-100 focus:outline-none hover:bg-opacity-25 focus:bg-gray-100 focus:bg-opacity-25 py-1 px-2 rounded" @click="expanded = false">Return</button>
+          </div>
         </div>
       </div>
     </div>
